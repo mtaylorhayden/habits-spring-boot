579c104903c0b0ab2c966ca1fc11a345d559ff60
Max Taylor-Hayden <nebraskamax@gmail.com>
2023-11-27 18:12:10 +0300

    Added documentation for habit

--- src/main/java/com/goalsapi/goalsapi/config/OpenApiConfig.java
+++ src/main/java/com/goalsapi/goalsapi/config/OpenApiConfig.java
@@ -3,10 +3,13 @@ package com.goalsapi.goalsapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
+@SecurityScheme(name = "Bearer Authenticatin", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
--- src/main/java/com/goalsapi/goalsapi/web/HabitController.java
+++ src/main/java/com/goalsapi/goalsapi/web/HabitController.java
@@ -18,41 +18,53 @@ import com.goalsapi.goalsapi.DTO.HabitDTO;
 import com.goalsapi.goalsapi.entity.Habit;
 import com.goalsapi.goalsapi.service.HabitService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 import javax.validation.Valid;
 
 // add swagger docs and authentication
 @Controller
 @RequestMapping(path = "/habit")
 public class HabitController {
+
     @Autowired
     HabitService habitService;
 
+    @Operation(summary = "Get all habits", description = "Gets all of the habits", security = @SecurityRequirement(name = "Bearer Authentication"))
     @GetMapping("/all")
     public ResponseEntity<List<Habit>> getAllHabits() {
         return new ResponseEntity<>(habitService.getHabits(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get single habit", description = "Gets a single by habit by the habitId", security = @SecurityRequirement(name = "Bearer Authentication"))
     @GetMapping("/{habitId}")
     public ResponseEntity<Habit> getHabitById(@PathVariable Long habitId) {
         return new ResponseEntity<>(habitService.getHabitById(habitId), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get all habits associated to a goal", description = "Gets a list of habits associated to a single goal by the goalId", security = @SecurityRequirement(name = "Bearer Authentication"))
     @GetMapping("/goal/{goalId}")
     public ResponseEntity<List<Habit>> getHabitsByGoalId(@PathVariable Long goalId) {
         return new ResponseEntity<>(habitService.getHabitsByGoalId(goalId),
                 HttpStatus.OK);
     }
 
+    @Operation(summary = "Creates a habit", description = "Creates a habit and adds it to a specific goal by goalId", security = @SecurityRequirement(name = "Bearer Authentication"))
     @PostMapping("/goal/{goalId}/add")
-    public ResponseEntity<Habit> addHabit(@Valid @RequestBody Habit habit, @PathVariable Long goalId) {
+    public ResponseEntity<Habit> addHabit(@Valid @RequestBody Habit habit,
+            @Parameter(description = "The habit will be associated with this goal") @PathVariable Long goalId) {
         return new ResponseEntity<>(habitService.saveHabit(habit, goalId), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update habit", description = "Updates a habit by the habitId", security = @SecurityRequirement(name = "Bearer Authentication"))
     @PutMapping("/{habitId}")
     public ResponseEntity<Habit> updateHabit(@PathVariable Long habitId, @Valid @RequestBody Habit habit) {
         return new ResponseEntity<>(habitService.updateHabit(habitId, habit), HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete habit", description = "Delete a habit by the habitId", security = @SecurityRequirement(name = "Bearer Authentication"))
     @DeleteMapping("/{habitId}")
     public ResponseEntity<HttpStatus> deleteHabit(@PathVariable Long habitId) {
         habitService.deleteHabit(habitId);
@@ -61,6 +73,7 @@ public class HabitController {
     }
 
     // DTO
+    @Operation(summary = "Get habit DTO", description = "Gets the habitDTO which only includes habitId, habit name, and goalId", security = @SecurityRequirement(name = "Bearer Authentication"))
     @GetMapping("/{habitId}/dto")
     public ResponseEntity<HabitDTO> getHabiDTOtById(@PathVariable Long habitId) {
         return new ResponseEntity<>(habitService.getHabitDTOById(habitId), HttpStatus.OK);
